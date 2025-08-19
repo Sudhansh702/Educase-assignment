8738b0bfe88affac162e8615f22e5805fc50bbb2
Sudhansh702 <snonu522@gmail.com>
2025-08-19 21:15:11 +0530

    Folder Structure Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter ,Routes , Route} from 'react-router-dom'
 import './App.css'
+
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import CreateAccount from './Pages/CreateAccount'
+import Setting from './Pages/Setting'
 
 function App() {
 
   return (
     <>
-      <h1 className=''>home</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<CreateAccount/>}/>
+          <Route path='/setting' element={<Setting/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/Pages/CreateAccount.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateAccount = () => {
+  return (
+    <div>
+      Register
+    </div>
+  )
+}
+
+export default CreateAccount
